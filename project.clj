e022a987c04908d5b5d0d249105a7b2d8fb965e3
Rick Moynihan <rick.m@swirrl.com>
2017-09-08 12:57:13 +0100

    Rename project artifact graphql-qb to match github repo

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject clj-graphql "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject graphql-qb "0.1.0-SNAPSHOT"
+  :description "Query RDF Datacubes with graphQL"
+  :url "http://swirrl.com/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
