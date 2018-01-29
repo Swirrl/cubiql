6ad725d90630413909e9a4f64e3753e3118f390e
lkitching <lee@swirrl.com>
2018-01-29 14:27:12 +0000

    Pass top-level schema key instead of direction when visiting schema model.

--- src/graphql_qb/schema_model.clj
+++ src/graphql_qb/schema_model.clj
@@ -70,14 +70,12 @@
 
 (declare visit-object)
 
-(defn visit-type [path type direction]
+(defn visit-type [path type type-schema-key]
   (cond
     (map? type)
-    (let [obj-result (visit-object path type direction)
+    (let [obj-result (visit-object path type type-schema-key)
           type-name (path->object-name path)
-          type-schema (if (= :output direction)
-                        {:objects {type-name (::object obj-result)}}
-                        {:input-objects {type-name (::object obj-result)}})]
+          type-schema {type-schema-key {type-name (::object obj-result)}}]
       {::schema (merge-schemas (::schema obj-result) type-schema)
        ::name   type-name})
 
@@ -89,12 +87,12 @@
 
     (vector? type)
     (let [type-def (first type)
-          element-type (visit-type path type-def direction)]
+          element-type (visit-type path type-def type-schema-key)]
       {::name (list 'list (::name element-type))
        ::schema (::schema element-type)})))
 
 (defn visit-arg [path {:keys [type] :as arg-def}]
-  (let [type-result (visit-type path type :input)
+  (let [type-result (visit-type path type :input-objects)
         out-arg (assoc arg-def :type (::name type-result))]
     {::schema (::schema type-result) ::arg out-arg}))
 
@@ -122,8 +120,8 @@
     :else
     (throw (IllegalArgumentException. "Expected fn or keyword for resolver"))))
 
-(defn visit-field [path field-name {:keys [type args resolve] :as field} direction]
-  (let [type-result (visit-type path type direction)
+(defn visit-field [path field-name {:keys [type args resolve] :as field} type-schema-key]
+  (let [type-result (visit-type path type type-schema-key)
         result {::field (assoc field :type (::name type-result)) ::schema (::schema type-result)}
         result (if (some? args)
                  (let [args-result (visit-args path args)]
@@ -141,10 +139,10 @@
     result))
 
 (defn visit-object
-  ([path object-def] (visit-object path object-def :output))
-  ([path {:keys [fields] :as object-def} direction]
+  ([path object-def] (visit-object path object-def :objects))
+  ([path {:keys [fields] :as object-def} type-schema-key]
    (let [fields-result (reduce (fn [acc [field-name field-def]]
-                              (let [result (visit-field (conj path field-name) field-name field-def direction)]
+                              (let [result (visit-field (conj path field-name) field-name field-def type-schema-key)]
                                 (-> acc
                                     (update ::schema merge-schemas (::schema result))
                                     (update-in [::fields] assoc field-name (::field result)))))
@@ -154,15 +152,13 @@
          out-fields (::fields fields-result)
          out-obj (assoc object-def :fields out-fields)
          obj-type-name (path->object-name path)
-         obj-schema (if (= direction :output)
-                      {:objects {obj-type-name out-obj}}
-                      {:input-objects {obj-type-name out-obj}})]
+         obj-schema {type-schema-key {obj-type-name out-obj}}]
      {::schema (merge-schemas schema obj-schema)
       ::object out-obj})))
 
 (defn visit-query [query-name {:keys [type resolve] :as query-def}]
   (let [path [query-name]
-        object-def (visit-object path type :output)
+        object-def (visit-object path type :objects)
         resolver-def (visit-resolver path resolve)]
     (merge-schemas
       (merge-schemas (::schema object-def) (::schema resolver-def))
