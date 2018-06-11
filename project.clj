efe56a6be78722f24b871665b8f6faf88dc70aba
Rick Moynihan <rick.m@swirrl.com>
2018-06-11 12:14:21 +0100

    Version 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject graphql-qb "0.2.0-SNAPSHOT"
+(defproject graphql-qb "0.2.0"
   :description "Query RDF Datacubes with graphQL"
   :url "http://swirrl.com/"
   :license {:name "Eclipse Public License"
