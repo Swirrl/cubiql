15d21ed7937f16a25c7e2a0dd12dfa245d003412
lkitching <lee@swirrl.com>
2018-10-26 15:35:25 +0100

    Version 0.5.0.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject graphql-qb "0.5.0-SNAPSHOT"
+(defproject graphql-qb "0.5.0"
   :description "Query RDF Datacubes with graphQL"
   :url "http://swirrl.com/"
   :license {:name "Eclipse Public License"
